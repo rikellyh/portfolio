6978382e3ce7f181b14b7058124e524d45ca2503
rikellyh <rikellyh898@gmail.com>
2025-03-03 21:59:53 -0300

    feat(Projects): add new project

--- src/components/Sections/Projects/Projects.tsx
+++ src/components/Sections/Projects/Projects.tsx
@@ -1,32 +1,60 @@
 import { GitHub } from "react-feather";
 
-import ProjectPicture from "../../../assets/img/notepay-img.png";
+import ProjectPicture1 from "../../../assets/img/notepay-img.png";
+import ProjectPicture2 from "../../../assets/img/huntington-gif.webp";
+
 import "./projects.css";
 
 export const Projects = () => {
   return (
-    <div className="projects__wrapper">
-      <div className="projects__wrapper--description">
-        <div>
-          <h1>Notepay</h1>
-          <span>(React + Typescript)</span>
+    <>
+      <div className="projects__wrapper">
+        <div className="projects__wrapper--description">
+          <div>
+            <h1>Notepay</h1>
+            <span>(React + Typescript)</span>
+          </div>
+          <p>
+            Projeto de duas páginas com o simples objetivo de calcular entradas
+            e saídas. Possui função de adicionar, editar e deletar itens da
+            lista.
+          </p>
         </div>
-        <p>
-          Projeto de duas páginas com o simples objetivo de calcular entradas e
-          saídas. Possui função de adicionar, editar e deletar itens da lista.
-        </p>
+        <div className="projects__wrapper--redirect">
+          <a href="https://github.com/rikellyh/notePay" target="_blank">
+            Ver código
+            <GitHub size={16} />
+          </a>
+        </div>
+        <div className="projects__wrapper--project">
+          <a href="https://github.com/rikellyh/notePay" target="_blank">
+            <img src={ProjectPicture1} alt="project notepay print" />
+          </a>
+        </div>
       </div>
-      <div className="projects__wrapper--redirect">
-        <a href="https://github.com/rikellyh/notePay" target="_blank">
-          Ver código
-          <GitHub size={16} />
-        </a>
+      <div className="projects__wrapper">
+        <div className="projects__wrapper--description">
+          <div>
+            <h1>Landing Page Huntington</h1>
+            <span>(Next + Typescript)</span>
+          </div>
+          <p>
+            Página única com TailwindCSS e componentes próprios (header, botões,
+            cards, accordion e footer) com dados mockados (textos e imagens).
+          </p>
+        </div>
+        <div className="projects__wrapper--redirect">
+          <a href="https://github.com/rikellyh/huntington-lp" target="_blank">
+            Ver código
+            <GitHub size={16} />
+          </a>
+        </div>
+        <div className="projects__wrapper--project">
+          <a href="https://github.com/rikellyh/huntington-lp" target="_blank">
+            <img src={ProjectPicture2} alt="project huntington-lp print" />
+          </a>
+        </div>
       </div>
-      <div className="projects__wrapper--project">
-        <a href="https://github.com/rikellyh/notePay" target="_blank">
-          <img src={ProjectPicture} alt="project notepay print" />
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
